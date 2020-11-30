ecf043c8bbe990af58a28ef1421f9c46b7e44ae0
Mike Terp <Primholt@hotmail.com>
2020-11-30 15:17:14 +0100

    sidedrawer responsive

--- frontend/src/components/side-drawer/side-drawer.component.jsx
+++ frontend/src/components/side-drawer/side-drawer.component.jsx
@@ -1,12 +1,11 @@
-import React, {useState} from 'react'
+import React from 'react'
 import './side-drawer.style.css'
 import {MenuItems} from '../../data/MenuItems'
-import {DropdownItems} from '../../data/DropdownItems'
+
 import {NavLink} from 'react-router-dom'
+import SideDrawerItem from './side-drawer-item.component'
 
-function SideDrawer(props)  {
-
- const [open, setOpen] = useState(false)
+const SideDrawer = (props) =>  {
 
  return (
         <nav className={props.show? 'side-drawer open' : 'side-drawer'}>
@@ -14,48 +13,9 @@ function SideDrawer(props)  {
                 <li><NavLink className="nav-links" exact to="/">Forside</NavLink>
                 </li>
                 {MenuItems.map((item, index) => {
-                if (item.hasChrildren) {
-                            return (
-                            <li key={index} className="hasChildren">
-                                <NavLink className={`${item.cName} child`} exact to={item.url} role="button">
-                                {<span>{item.title} {<div className="toggle-submenu">
-                                    <i class="fas fa-angle-right" className="fas fa-angle-right icon-sidedrawer" onClick={() => setOpen(!open)}></i> </div>} 
-                                    </span>}  
-                                </NavLink>
-                                
-                        <div className={(open)? "sub-wrapper open" : "sub-wrapper"}>
-                                <ul className="sub-list">
-                                    <li className="go-back" onClick={() => setOpen(!open)}>
-                                    <div className="testing"><i class="fas fa-angle-left"></i>
-                                    <text className="back-btn-text">Tilbage</text>
-                                    </div>
-                                   
-                                    </li>
-                                    <li><text className="heading-sub-menu"  >{item.title}</text></li>
-                                    {DropdownItems.map((childItem, childIndex) =>{
-                                        return(
-                                        <li key={childIndex}>
-                                            <NavLink className={childItem.cName} exact to={childItem.url}>
-                                                <span>{childItem.title}</span>  
-                                            </NavLink>
-                                        </li>
-                                        )
-                                    })}
-                                </ul>
-                                    </div>
-                            </li>
-                            )
-                            }
-							else { 
-                                return(
-                                    <li key={index}>
-                                        <NavLink className={item.cName} exact to={item.url} role="button">
-                                            {item.title}
-                                        </NavLink>
-                                    </li>
-                                )
-                            }
-                        })
+                    return(
+                    <SideDrawerItem key={index} item={item}></SideDrawerItem>
+                )})
                     }
             </ul>
         </nav>
