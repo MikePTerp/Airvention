4ebd56dc81f80a8c64e8b2c892a8d2d654c72928
Simon Skole <funaielc@hotmail.com>
2020-12-03 10:32:59 +0100

    Working

--- frontend/src/pages/hompage/homepage.component.jsx
+++ frontend/src/pages/hompage/homepage.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import HeroSection from '../../hero/hero-section.component'
-import Checkmarks from '../../checkmarks/checkmarks.component'
-import Collaborations from '../../collaborations/collaborations.component'
+import HeroSection from '../../components/hero/hero-section.component'
+import Checkmarks from '../../components/checkmarks/checkmarks.component'
+import Collaborations from '../../components/collaborations/collaborations.component'
 
  const Homepage = () => {
     return (
